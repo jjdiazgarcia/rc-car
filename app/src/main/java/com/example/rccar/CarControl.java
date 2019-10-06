ff2460673f9a1f5e9dbe6c0e9509b47c1b435853
Jerónimo Díaz <jeronimo.telec@gmail.com>
2019-10-06 18:32:37 +0200

    Customize car directions indicators, background and battery level images

--- app/src/main/java/com/example/rccar/CarControl.java
+++ app/src/main/java/com/example/rccar/CarControl.java
@@ -3,12 +3,46 @@ package com.example.rccar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class CarControl extends AppCompatActivity {
+
+    private ImageButton forwardButton, backwardButton, leftButton, rightButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_control);
+
+        forwardButton =(ImageButton)findViewById(R.id.forward_button);
+        forwardButton.setOnClickListener(new View.OnClickListener()   {
+            public void onClick(View v)  {
+                Toast.makeText(getApplicationContext(), "Forward button", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        backwardButton =(ImageButton)findViewById(R.id.backward_button);
+        backwardButton.setOnClickListener(new View.OnClickListener()   {
+            public void onClick(View v)  {
+                Toast.makeText(getApplicationContext(), "Backward button", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        leftButton =(ImageButton)findViewById(R.id.left_button);
+        leftButton.setOnClickListener(new View.OnClickListener()   {
+            public void onClick(View v)  {
+                Toast.makeText(getApplicationContext(), "Left button", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        rightButton =(ImageButton)findViewById(R.id.right_button);
+        rightButton.setOnClickListener(new View.OnClickListener()   {
+            public void onClick(View v)  {
+                Toast.makeText(getApplicationContext(), "Right button", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
